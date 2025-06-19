62e9c093c9aca6ca1131be30ee14b5f42bc542e7
Mayank <48953181+logic-01@users.noreply.github.com>
2025-06-19 12:23:19 +0530

    Updated 21s.js

---
+++ 21s/21s.js
@@ -0,0 +1,16 @@
+//create deck
+//distribute 2 cards to sam and dealer each
+//start game --> check for total 21 for both
+//has 21? game end, the player having 21 won.
+//sam starts drawing the cards till total is 17 or higher
+
+const Sam =[];
+const Dealer =[];
+const Deck=[]
+function startGame(){
+
+}
+
+function createAndDistributeCards(){
+  
+}
